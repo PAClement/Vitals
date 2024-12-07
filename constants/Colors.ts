ee8df3769136e39ca0a9f81751029d72804fdefc
clement <clementpaquentin@hotmail.fr>
2024-12-07 05:47:58 +0100

    feat(constant): add colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,7 +2,9 @@ export const Colors = {
     light: {
         primary: "#4b0082",
         secondary: "#5856D6",
-        grayWhite: "#c8c8cc",
+        text: "#000",
+        grayWhite: "#efefef",
+        white: "#fff",
         success: "#4CD964",
         warning: "#FFCC00",
         danger: "#FF3B30",
@@ -10,7 +12,9 @@ export const Colors = {
     dark: {
         primary: "#4b0082",
         secondary: "#5856D6",
-        grayWhite: "#c8c8cc",
+        text: "#000",
+        grayWhite: "#efefef",
+        white: "#fff",
         success: "#4CD964",
         warning: "#FFCC00",
         danger: "#FF3B30",
