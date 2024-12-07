bfd0bebfb8e8e2c8e6b61cc8dab9d587cf69076e
clement <clementpaquentin@hotmail.fr>
2024-12-07 05:48:49 +0100

    feat(app): add Account page

---
+++ app/(tabs)/Account.tsx
@@ -0,0 +1,10 @@
+import {ThemedText} from "@/components/ThemedText";
+import RootView from "@/components/RootView";
+
+export default function Index() {
+    return (
+        <RootView>
+            <ThemedText variant="headline">Votre Compte</ThemedText>
+        </RootView>
+    );
+}
