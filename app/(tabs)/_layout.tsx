ca765da0e35c0ed4efce4343afc476c1b319a35d
PAClement <clementpaquentin@hotmail.fr>
2024-12-08 23:07:16 +0100

    feat(app): add focused property

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import {Tabs} from 'expo-router';
 import {useThemeColors} from "@/hooks/useThemeColors";
+import {View} from "react-native";
 
 export default function TabLayout() {
     const colors = useThemeColors();
@@ -12,14 +13,18 @@ export default function TabLayout() {
                 name="index"
                 options={{
                     title: '',
-                    tabBarIcon: ({color}) => <Ionicons name="home" size={24} color={colors.primary}/>,
+                    tabBarIcon: ({focused}) =>
+                        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                            <Ionicons name="home" size={24} color={focused ? colors.primary : colors.text}/>
+                        </View>,
                 }}
             />
             <Tabs.Screen
                 name="Account"
                 options={{
                     title: '',
-                    tabBarIcon: ({color}) => <FontAwesome name="user-circle-o" size={24} color={colors.primary}/>,
+                    tabBarIcon: ({focused}) => <FontAwesome name="user-circle-o" size={24}
+                                                            color={focused ? colors.primary : colors.text}/>,
                 }}
             />
             <Tabs.Screen
