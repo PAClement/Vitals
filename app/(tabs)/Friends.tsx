bf262f95a2a428c1ac45196bef5190bb03577c98
PAClement <clementpaquentin@hotmail.fr>
2024-12-08 23:20:29 +0100

    feat(app): add friends page

---
+++ app/(tabs)/Friends.tsx
@@ -0,0 +1,10 @@
+import {ThemedText} from "@/components/ThemedText";
+import RootView from "@/components/RootView";
+
+export default function Friends() {
+    return (
+        <RootView>
+            <ThemedText variant="headline">Friends</ThemedText>
+        </RootView>
+    );
+}
