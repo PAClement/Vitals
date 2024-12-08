a980ded8a8219b6476e77db1e4d4add48e68d33c
PAClement <clementpaquentin@hotmail.fr>
2024-12-08 23:19:22 +0100

    feat(app): add account information

--- app/(tabs)/Account.tsx
+++ app/(tabs)/Account.tsx
@@ -1,10 +1,23 @@
 import {ThemedText} from "@/components/ThemedText";
 import RootView from "@/components/RootView";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import {StyleSheet, View} from "react-native";
 
 export default function Index() {
     return (
         <RootView>
-            <ThemedText variant="headline">Votre Compte</ThemedText>
+            <View style={styles.header}>
+                <ThemedText variant="headline">Votre Compte</ThemedText>
+                <AntDesign name="setting" size={24} color="black"/>
+            </View>
         </RootView>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    }
+})
