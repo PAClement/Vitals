99026b345f3a999e3c007b2ea565e2b63d90bfa0
clement <clementpaquentin@hotmail.fr>
2024-12-09 17:40:33 +0100

    feat(app): add modal in Friends page

--- app/(tabs)/Friends.tsx
+++ app/(tabs)/Friends.tsx
@@ -1,10 +1,42 @@
 import {ThemedText} from "@/components/ThemedText";
 import RootView from "@/components/RootView";
+import {Pressable, StyleSheet, View} from "react-native";
+import {useThemeColors} from "@/hooks/useThemeColors";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Modal from "@/components/Modal";
+import {useState} from "react";
 
 export default function Friends() {
+    const colors = useThemeColors();
+    const [addFriendModal, setAddFriendModal] = useState(false);
+
     return (
         <RootView>
-            <ThemedText variant="headline">Friends</ThemedText>
+            <View style={styles.header}>
+                <ThemedText variant="headline">Amis</ThemedText>
+                <Pressable onPress={() => setAddFriendModal(true)}
+                           style={[styles.addFriendButton, {backgroundColor: colors.secondary}]}>
+                    <AntDesign name="adduser" size={24} color={colors.white}/>
+                </Pressable>
+            </View>
+            <Modal id="test" isOpen={addFriendModal}>
+                <ThemedText variant="body">Hello</ThemedText>
+                <Pressable onPress={() => setAddFriendModal(false)}>
+
+                </Pressable>
+            </Modal>
         </RootView>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    addFriendButton: {
+        padding: 8,
+        borderRadius: 50,
+    }
+});
