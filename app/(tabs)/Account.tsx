7dd5fb481f2df0503c9d5100a20e30ce7ada919b
clement <clementpaquentin@hotmail.fr>
2024-12-09 17:40:14 +0100

    feat(app): add pressable button

--- app/(tabs)/Account.tsx
+++ app/(tabs)/Account.tsx
@@ -1,14 +1,16 @@
 import {ThemedText} from "@/components/ThemedText";
 import RootView from "@/components/RootView";
 import AntDesign from '@expo/vector-icons/AntDesign';
-import {StyleSheet, View} from "react-native";
+import {Pressable, StyleSheet, View} from "react-native";
 
 export default function Index() {
     return (
         <RootView>
             <View style={styles.header}>
                 <ThemedText variant="headline">Votre Compte</ThemedText>
-                <AntDesign name="setting" size={24} color="black"/>
+                <Pressable>
+                    <AntDesign name="setting" size={24} color="black"/>
+                </Pressable>
             </View>
         </RootView>
     );
