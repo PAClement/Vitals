362ad89a1b628c13d7bae3da7215aa99c7d098b3
clement <clementpaquentin@hotmail.fr>
2024-12-11 02:16:30 +0100

    fix(component): fix modal component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,26 +1,58 @@
-import {View, Modal as RNModal, ModalProps} from 'react-native';
-import {SafeAreaView} from "react-native-safe-area-context";
-import {useState} from "react";
+import {Dimensions, Modal as RNModal, ModalProps, Pressable, View, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from "react";
+import {Shadows} from "@/constants/Shadows";
+import {useThemeColors} from "@/hooks/useThemeColors";
 
 type props = ModalProps & {
     isOpen: boolean;
+    onClose: () => void;
 }
 
-export default function Modal({isOpen, children, ...rest}: props) {
+export default function Modal({isOpen, onClose, children, ...rest}: props) {
 
-    const [isVisible, setIsVisible] = useState(isOpen);
+    const [isVisible, setIsVisible] = useState(false);
+    const [position, setPosition] = useState<null | { top: number, right: number }>(null)
+
+    const colors = useThemeColors();
+
+    useEffect(() => {
+        stateModal(isOpen);
+    }, [isOpen]);
+
     const stateModal = (state: boolean) => {
+        const {height} = Dimensions.get('window');
+        if (state) {
+            setPosition({top: height / 2, right: 10});
+        }
         setIsVisible(state);
     };
 
     return (
-        <SafeAreaView>
-            <RNModal visible={isVisible} transparent animationType={'fade'} onRequestClose={() => stateModal(false)}
-                     statusBarTranslucent {...rest}>
-                <View>
-                    {children}
-                </View>
-            </RNModal>
-        </SafeAreaView>
+        <RNModal transparent visible={isVisible} onRequestClose={() => {
+            stateModal(false);
+            onClose();
+        }}  {...rest}>
+            <Pressable style={styles.backdrop} onPress={() => {
+                stateModal(false);
+                onClose();
+            }}></Pressable>
+            <View style={[styles.popup, {backgroundColor: colors.white, ...position}]}>
+                {children}
+            </View>
+        </RNModal>
     );
 };
+
+const styles = StyleSheet.create({
+    popup: {
+        position: 'absolute',
+        width: '95%',
+        padding: 4,
+        borderRadius: 12,
+        ...Shadows.dp2
+    },
+    backdrop: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    }
+});
