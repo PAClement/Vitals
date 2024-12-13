d99733014f32cd3475c5e6ef891b0618dd8905ea
PAClement <clementpaquentin@hotmail.fr>
2024-12-13 16:46:00 +0100

    feat(component): add lin kand pressable button

--- components/FriendCard.tsx
+++ components/FriendCard.tsx
@@ -6,6 +6,7 @@ import {Card} from "@/components/Card";
 import React from "react";
 import {useThemeColors} from "@/hooks/useThemeColors";
 import ButtonManageFriends from "@/components/friend/ButtonManageFriends";
+import {Link} from "expo-router";
 
 type Props = {
     id: number,
@@ -15,33 +16,36 @@ type Props = {
 export default function FriendCard({id, state}: Props) {
     const colors = useThemeColors();
 
-    return <Card style={[styles.Card, (state === 'process') && {borderWidth: 1, borderColor: colors.warning}]}>
-        <Row style={{justifyContent: 'space-between'}}>
-            <Row gap={8}>
-                <AntDesign name="user" size={24} color={colors.secondary}/>
-                <ThemedText variant="subtitle1">Friend's name </ThemedText>
-            </Row>
-            {state === 'process' ? (
-                <Row gap={8}>
-                    <ButtonManageFriends id={id} state={'accepted'} title={'Accepter'} color={colors.success}
-                                         accessibilityLabel={"Accepter la demande d'amis"}/>
-                    <ButtonManageFriends id={id} state={'reject'} title={'Rejeter'} color={colors.warning}
-                                         accessibilityLabel={"Rejeter la demande d'amis"}/>
+    return <Link href={{pathname: "/user/[id]", params: {id: id}}} asChild>
+        <Pressable android_ripple={{color: colors.secondary, foreground: true}} style={{width: '100%'}}>
+            <Card style={[styles.Card, (state === 'process') && {borderWidth: 1, borderColor: colors.warning}]}>
+                <Row style={{justifyContent: 'space-between'}}>
+                    <Row gap={8}>
+                        <AntDesign name="user" size={24} color={colors.secondary}/>
+                        <ThemedText variant="subtitle1">Friend's name </ThemedText>
+                    </Row>
+                    {state === 'process' ? (
+                        <Row gap={8}>
+                            <ButtonManageFriends id={id} state={'accepted'} title={'Accepter'} color={colors.success}
+                                                 accessibilityLabel={"Accepter la demande d'amis"}/>
+                            <ButtonManageFriends id={id} state={'reject'} title={'Rejeter'} color={colors.warning}
+                                                 accessibilityLabel={"Rejeter la demande d'amis"}/>
+                        </Row>
+                    ) : (
+                        <Pressable>
+                            <AntDesign name="eyeo" size={24} color={colors.primary}/>
+                        </Pressable>
+                    )}
                 </Row>
-            ) : (
-                <Pressable>
-                    <AntDesign name="eyeo" size={24} color={colors.primary}/>
-                </Pressable>
-            )}
-        </Row>
-    </Card>
+            </Card>
+        </Pressable>
+    </Link>
 
 }
 
 const styles = StyleSheet.create({
     Card: {
         padding: 15,
-        width: '100%'
     },
     actionButton: {
         fontSize: 12
