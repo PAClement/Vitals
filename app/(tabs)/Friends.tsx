29d057a4569e250385b63e8bff634885af348f09
PAClement <clementpaquentin@hotmail.fr>
2024-12-13 11:37:16 +0100

    feat(app): add Friend Card component

--- app/(tabs)/Friends.tsx
+++ app/(tabs)/Friends.tsx
@@ -1,6 +1,6 @@
 import {ThemedText} from "@/components/ThemedText";
 import RootView from "@/components/RootView";
-import {Pressable, StyleSheet} from "react-native";
+import {Pressable, StyleSheet, View} from "react-native";
 import Modal from "@/components/Modal";
 import {useThemeColors} from "@/hooks/useThemeColors";
 import AntDesign from '@expo/vector-icons/AntDesign';
@@ -8,6 +8,7 @@ import {useState} from "react";
 import Row from "@/components/Row";
 import React from "react";
 import SearchBar from "@/components/SearchBar";
+import FriendCard from "@/components/FriendCard";
 
 export default function Friends() {
     const [addFriendModal, setAddFriendModal] = useState(false);
@@ -27,16 +28,32 @@ export default function Friends() {
     return (
         <>
             <RootView>
-                <Row style={[styles.header, {marginBottom: 15}]}>
-                    <ThemedText variant="headline">Vos proches</ThemedText>
-                    <Pressable onPress={openAddFriendModal}
-                               style={[styles.addFriendButton, {backgroundColor: colors.secondary}]}>
-                        <AntDesign name="adduser" size={24} color={colors.white}/>
-                    </Pressable>
-                </Row>
-                <Row>
-                    <SearchBar value={searchFriend} onChange={setSearchFriend}></SearchBar>
-                </Row>
+                <View style={{marginBottom: 15}}>
+                    <Row style={[styles.header, {marginBottom: 15}]}>
+                        <ThemedText variant="headline">Vos proches</ThemedText>
+                        <Pressable onPress={openAddFriendModal}
+                                   style={[styles.addFriendButton, {backgroundColor: colors.secondary}]}>
+                            <AntDesign name="adduser" size={24} color={colors.white}/>
+                        </Pressable>
+                    </Row>
+                    <Row>
+                        <SearchBar value={searchFriend} onChange={setSearchFriend}></SearchBar>
+                    </Row>
+                </View>
+                <View style={{gap:30}}>
+                    <View style={{gap: 10}}>
+                        <ThemedText variant="subtitle1">Demande en attente</ThemedText>
+                        <Row>
+                            <FriendCard id={10} state={'process'}/>
+                        </Row>
+                    </View>
+                    <View style={{gap: 10}}>
+                        <ThemedText variant="subtitle1">Proches (10)</ThemedText>
+                        <Row>
+                            <FriendCard id={10} state={'accepted'}/>
+                        </Row>
+                    </View>
+                </View>
             </RootView>
             <Modal isOpen={addFriendModal} onClose={closeAddFriendModal} position="center">
                 <ThemedText variant="subtitle1">Ajouter</ThemedText>
