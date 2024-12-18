17cc3468a9e94e4c40f174d2731ebd787c1dbc9b
clement <clementpaquentin@hotmail.fr>
2024-12-18 21:49:03 +0100

    feat(app): add create group modal

--- app/(tabs)/Groups.tsx
+++ app/(tabs)/Groups.tsx
@@ -2,23 +2,30 @@ import RootView from "@/components/RootView";
 import Row from "@/components/Row";
 import {ThemedText} from "@/components/ThemedText";
 import {Pressable, StyleSheet} from "react-native";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import React from "react";
+import React, {useState} from "react";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import {useThemeColors} from "@/hooks/useThemeColors";
+import Modal from "@/components/Modal";
 
 export default function Groups() {
+    const [createGroupModal, setCreateGroupModal] = useState(false);
     const colors = useThemeColors();
 
     return (
-        <RootView>
-            <Row style={[styles.header, {marginBottom: 15}]}>
-                <ThemedText variant="headline">Groupe</ThemedText>
-                <Pressable style={[styles.addGroupButton, {backgroundColor: colors.secondary}]}>
-                    <MaterialCommunityIcons name="layers-plus" size={24} color={colors.white} />
-                </Pressable>
-            </Row>
-        </RootView>
+        <>
+            <RootView>
+                <Row style={[styles.header, {marginBottom: 15}]}>
+                    <ThemedText variant="headline">Groupe</ThemedText>
+                    <Pressable onPress={() => setCreateGroupModal(true)}
+                               style={[styles.addGroupButton, {backgroundColor: colors.secondary}]}>
+                        <MaterialCommunityIcons name="layers-plus" size={24} color={colors.white}/>
+                    </Pressable>
+                </Row>
+            </RootView>
+            <Modal isOpen={createGroupModal} onClose={() => setCreateGroupModal(false)} position="center">
+                <ThemedText variant="subtitle1">Create groupe</ThemedText>
+            </Modal>
+        </>
     )
 }
 
