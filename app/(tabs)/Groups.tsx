b6efc541aa3cfb60a6a2b3c3d4a762f1af714cfc
clement <clementpaquentin@hotmail.fr>
2024-12-18 21:39:52 +0100

    refactor(app): add title and add group button

--- app/(tabs)/Groups.tsx
+++ app/(tabs)/Groups.tsx
@@ -1,13 +1,34 @@
 import RootView from "@/components/RootView";
 import Row from "@/components/Row";
 import {ThemedText} from "@/components/ThemedText";
+import {Pressable, StyleSheet} from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import React from "react";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import {useThemeColors} from "@/hooks/useThemeColors";
 
 export default function Groups() {
+    const colors = useThemeColors();
+
     return (
         <RootView>
-            <Row>
-                <ThemedText>Groups</ThemedText>
+            <Row style={[styles.header, {marginBottom: 15}]}>
+                <ThemedText variant="headline">Groupe</ThemedText>
+                <Pressable style={[styles.addGroupButton, {backgroundColor: colors.secondary}]}>
+                    <MaterialCommunityIcons name="layers-plus" size={24} color={colors.white} />
+                </Pressable>
             </Row>
         </RootView>
     )
 }
+
+const styles = StyleSheet.create({
+    header: {
+        padding: 1,
+        justifyContent: 'space-between',
+    },
+    addGroupButton: {
+        padding: 8,
+        borderRadius: 50,
+    },
+});
