8aca02d22c5400f5958ba1bad074f110bca3031b
clement <clementpaquentin@hotmail.fr>
2025-01-08 22:45:03 +0100

    feat(view): update button view

--- app/(app)/(tabs)/Settings.tsx
+++ app/(app)/(tabs)/Settings.tsx
@@ -2,20 +2,31 @@ import Row from "@/components/Row";
 import {ThemedText} from "@/components/ThemedText";
 import {useThemeColors} from "@/hooks/useThemeColors";
 import RootView from "@/components/RootView";
-import {Button} from "react-native";
+import {Button, StyleSheet} from "react-native";
 import {useSession} from "@/ctx";
+import React from "react";
 
 export default function Settings() {
     const colors = useThemeColors();
-    const { signOut } = useSession();
+    const {signOut} = useSession();
 
 
     return (
         <RootView>
-            <Row>
-                <ThemedText>Settings</ThemedText>
+            <Row style={[styles.header, {marginBottom: 15}]}>
+                <ThemedText variant="headline">Groupe</ThemedText>
             </Row>
-            <Button onPress={signOut} title={'Disconnected'}></Button>
+            <Row>
+                <ThemedText variant="body">Se déconnecter</ThemedText>
+                <Button onPress={signOut} title={'Disconnected'} color={colors.danger}></Button>
+            </Row>
         </RootView>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        padding: 1,
+        justifyContent: 'space-between',
+    },
+});
