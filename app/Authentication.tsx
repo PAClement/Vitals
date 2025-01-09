872f2f11b8606dac7118ddf6bf9776c5e1f04465
PAClement <clementpaquentin@hotmail.fr>
2025-01-09 13:33:24 +0100

    feat(app): wip logging form

--- app/Authentication.tsx
+++ app/Authentication.tsx
@@ -1,16 +1,35 @@
 import {router} from 'expo-router';
-import {Button, Text, View} from 'react-native';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
 
 import {useSession} from '@/ctx';
+import RootView from "@/components/RootView";
+import {ThemedText} from "@/components/ThemedText";
 
 export default function SignIn() {
     const {signIn} = useSession();
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <RootView style={{gap: 15}}>
+            <ThemedText variant={"headline"} align={'center'}>Vitals</ThemedText>
+            <View>
+                <TextInput keyboardType={'email-address'} inputMode={'email'} style={styles.input}
+                           placeholder={'Email'}></TextInput>
+                <TextInput keyboardType={'default'} inputMode={'text'} secureTextEntry={true} style={styles.input}
+                           placeholder={'Mot de passe'}></TextInput>
+            </View>
             <Button onPress={() => {
                 signIn();
                 router.replace('/');
             }} title={'Click to login'}></Button>
-        </View>
+        </RootView>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10
+    }
+});
